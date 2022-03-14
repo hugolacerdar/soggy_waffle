bc80d487f1177d888e7bb122c1ed7967d535fd54
Hugo <hugolacerdar@gmail.com>
2022-03-14 12:41:14 -0300

    chore: add fake weather api for tests

---
+++ lib/soggy_waffle/fake_weather_api.ex
@@ -0,0 +1,19 @@
+defmodule SoggyWaffle.FakeWeatherAPI do
+  require Logger
+
+  @spec get_forecast(String.t()) :: {:ok, map()}
+  def get_forecast(city) do
+    _ = Logger.info("Getting forecast for city: #{city}")
+
+    response = %{
+      "list" => [
+        %{
+          "dt" => DateTime.to_unix(DateTime.utc_now()),
+          "weather" => [%{"id" => _thunderstorm = 231}]
+        }
+      ]
+    }
+
+    {:ok, response}
+  end
+end
