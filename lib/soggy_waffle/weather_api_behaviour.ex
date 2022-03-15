4a7d075550905c4e91fe06785be45429ed0ddbe6
Hugo <hugolacerdar@gmail.com>
2022-03-15 20:10:27 -0300

    chore: add get_forecast/2 callback

--- lib/soggy_waffle/weather_api_behaviour.ex
+++ lib/soggy_waffle/weather_api_behaviour.ex
@@ -1,3 +1,4 @@
 defmodule SoggyWaffle.WeatherAPI.Behaviour do
   @callback get_forecast(city :: String.t()) :: {:ok, term()} | {:error, reason :: term()}
+  @callback get_forecast(city :: String.t(), base_url :: String.t()) :: {:ok, term()} | {:error, reason :: term()}
 end
