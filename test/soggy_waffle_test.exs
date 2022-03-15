da3016b272eb26f7e874ff78fd5c8d5577c61735
Hugo <hugolacerdar@gmail.com>
2022-03-15 11:41:27 -0300

    test: call Mox.expect to assert get_forest is called only once

--- test/soggy_waffle_test.exs
+++ test/soggy_waffle_test.exs
@@ -1,38 +1,28 @@
 defmodule SoggyWaffleTest do
 
   use ExUnit.Case
-  import ExUnit.CaptureLog
+  import Mox
 
   describe "rain?/2" do
     test "success: gets forecasts, returns true for imminent rain" do
-      fun = fn ->
-        SoggyWaffle.rain?("Los Angeles", DateTime.utc_now())
-      end
+      expect(SoggyWaffle.WeatherAPIMock, :get_forecast, 1, fn city ->
+        assert city == "Los Angeles"
 
-      assert capture_log(fun) =~ "Getting forecast for city: Los Angeles"
+        response = %{
+          "list" => [
+            %{
+              "dt" => DateTime.to_unix(DateTime.utc_now()) + 60,
+              "weather" => [%{"id" => _thunderstorm = 231}]
+            }
+          ]
+        }
+
+        {:ok, response}
+      end)
+
+      assert SoggyWaffle.rain?("Los Angeles", DateTime.utc_now())
+
+      verify!(SoggyWaffle.WeatherAPIMock)
     end
-    # test "success: gets forecasts, returns true for imminent rain" do
-    #   now = DateTime.utc_now()
-    #   future_unix = DateTime.to_unix(now) + 1
-    #   expected_city = Enum.random(["Denver", "Los Angeles", "New York"])
-    #   test_pid = self()
-
-    #   weather_fn_double = fn city ->
-    #     send(test_pid, {:get_forecast_called, city})
-
-    #     data = [
-    #       %{
-    #         "dt" => future_unix,
-    #         "weather" => [%{"id" => _drizzle_id = 300}]
-    #       }
-    #     ]
-
-    #     {:ok, %{"list" => data}}
-    #   end
-
-    #   assert SoggyWaffle.rain?(expected_city, now, weather_fn_double)
-
-    #   assert_received {:get_forecast_called, ^expected_city}, "get_forecast/1 was never called"
-    # end
   end
 end
