e565235e1bd69ae7631f9e27267ec2d83034fb3f
Hugo <hugolacerdar@gmail.com>
2022-03-28 14:05:11 -0300

    test:  fix test

--- test/soggy_waffle_test.exs
+++ test/soggy_waffle_test.exs
@@ -5,6 +5,29 @@ defmodule SoggyWaffleTest do
 
   setup :verify_on_exit!
 
+  setup do
+    current_weather_api_module =
+      Application.get_env(
+        :soggy_waffle,
+        :weather_api_module,
+        SoggyWaffle.WeatherAPI
+      )
+
+    Application.put_env(
+      :soggy_waffle,
+      :weather_api_module,
+      SoggyWaffle.WeatherAPIMock
+    )
+
+    on_exit(fn ->
+      Application.put_env(
+        :soggy_waffle,
+        :weather_api_module,
+        current_weather_api_module
+      )
+    end)
+    :ok
+  end
   describe "rain?/2" do
 
     setup _ do
@@ -13,23 +36,22 @@ defmodule SoggyWaffleTest do
     end
     test "success: gets forecasts, returns true for imminent rain" do
 
-      # expect(SoggyWaffle.WeatherAPIMock, :get_forecast, fn city ->
-      #   IO.inspect(city == "Los Angeles", label: "Checking")
-      #   assert city == "Los Angeles"
+      expect(SoggyWaffle.WeatherAPIMock, :get_forecast, fn city ->
+        assert city == "Los Angeles"
 
-      #   response = %{
-      #     "list" => [
-      #       %{
-      #         "dt" => DateTime.to_unix(DateTime.utc_now()) + 60,
-      #         "weather" => [%{"id" => _thunderstorm = 231}]
-      #       }
-      #     ]
-      #   }
+        response = %{
+          "list" => [
+            %{
+              "dt" => DateTime.to_unix(DateTime.utc_now()) + 60,
+              "weather" => [%{"id" => _thunderstorm = 231}]
+            }
+          ]
+        }
 
-      #   {:ok, response}
-      # end)
+        {:ok, response}
+      end)
 
-      # assert SoggyWaffle.rain?("Los Angeles", DateTime.utc_now())
+      assert SoggyWaffle.rain?("Los Angeles", DateTime.utc_now())
     end
   end
 end
