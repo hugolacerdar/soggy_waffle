059dd6e89b7124b51b336390aaf2f83a7ea790b7
Hugo <hugolacerdar@gmail.com>
2022-03-28 11:48:11 -0300

    refactor: improve performance using compile_env

--- lib/soggy_waffle.ex
+++ lib/soggy_waffle.ex
@@ -7,15 +7,14 @@
 # Visit http://www.pragmaticprogrammer.com/titles/lmelixir for more book information.
 #---
 defmodule SoggyWaffle do
-  def rain?(city, datetime) do
-    weather_api_module =
-      Application.get_env(
-        :soggy_waffle,
-        :weather_api_module,
-        SoggyWaffle.WeatherAPI
-      )
+  @weather_api_module Application.compile_env(
+    :soggy_waffle,
+    :weather_api_module,
+    SoggyWaffle.WeatherAPI
+  )
 
-    with {:ok, response} <- weather_api_module.get_forecast(city) do
+  def rain?(city, datetime) do
+    with {:ok, response} <- @weather_api_module.get_forecast(city) do
       {:ok, weather_data} =
         SoggyWaffle.WeatherAPI.ResponseParser.parse_response(response)
 
